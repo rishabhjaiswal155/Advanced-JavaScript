ad66741d33b29c8ed530321a469f1f3a7cad216b
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-11 15:27:25 +0530

    Function Currying in JavaScript

--- main.js
+++ main.js
@@ -14,16 +14,44 @@
 
 //Closures (It Bundle the function def and scope and store the prev state)
 
-function outer(){
-    let counter=0
-    function inner(){
-        counter++
-        console.log(counter)
-    }
-    return inner
+// function outer(){
+//     let counter=0
+//     function inner(){
+//         counter++
+//         console.log(counter)
+//     }
+//     return inner
+// }
+// const fn=outer()
+// fn()
+// fn()
+// fn()
+
+
+//Function Currying: function with multiple argument is transformed in function with single arg
+//sum(a,b,c)====>>sum(a)(b)(c)===> this is currying
+function sum(a,b,c){
+    return a+b+c
 }
-const fn=outer()
-fn()
-fn()
-fn()
+
+//console.log(sum(2,3,5))
+
+function curry(fn){
+    return function(a){
+        return function(b){
+            return function(c){
+                return fn(a,b,c)
+            }
+        }
+    }
+}
+
+const curriedSum=curry(sum)
+console.log(curriedSum(2)(3)(5))
+//OR
+const add2=curriedSum(2)
+const add3=add2(3)
+const add5=add3(5)
+console.log(add5)
+
 
